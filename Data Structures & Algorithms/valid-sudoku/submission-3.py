42c842cdb1344c0c5678891159a97c9c3193e754
Brian P <32627525+brianhip@users.noreply.github.com>
2026-08-28 11:54:32 -0600

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # build list of cols and sections top left to bottom right
+        ROWS, COLS, BOXS = self.buildRowsColsBoxes(board)
+        return self.validation(ROWS, COLS, BOXS)
+        
+
+    def buildRowsColsBoxes(self, board: List[List[str]]) -> List[List]:
+        ROWS = [["." for j in range(9)] for i in range(9)]
+        COLS = [["." for j in range(9)] for i in range(9)]
+        BOXS = [["." for j in range(9)] for i in range(9)] 
+
+        for i in range(9):
+            for j in range(9):
+                val = board[i][j]
+                if val == ".":
+                    continue
+                ROWS[i][j] = val
+                COLS[j][i] = val
+                BOXS[(i // 3 * 3) + j // 3][(i % 3 * 3)+ j % 3] = val 
+        return [ROWS, COLS, BOXS]
+    
+    def validation(self, ROWS, COLS, BOXS) -> bool:
+        for i in range(1, 10):
+            number = str(i)
+            for j in range(9):
+                r_occurrences = ROWS[j].count(number)
+                c_occurrences = COLS[j].count(number)
+                b_occurrences = BOXS[j].count(number)
+                if r_occurrences > 1 or c_occurrences > 1 or b_occurrences > 1:
+                    return False
+        return True
