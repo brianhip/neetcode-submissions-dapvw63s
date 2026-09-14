e79e5ef040f8bf0de4b8ae2c06c6313e9d6557ff
Brian P <32627525+brianhip@users.noreply.github.com>
2026-09-14 10:50:59 -0600

    Add: evaluate-division - submission-1

---
+++ Data Structures & Algorithms/evaluate-division/submission-1.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        bidirectional_weighted_graph = defaultdict(dict)
+        bwg = bidirectional_weighted_graph
+        for i in range(len(equations)):
+            first, second = equations[i]
+            bwg[first][second] = values[i]
+            bwg[second][first] = 1/values[i]
+
+        def dfs(curr, goal, curr_ratio, visited):
+            if curr in visited or curr not in bwg:
+                return -1.0
+            visited.add(curr)
+            if curr == goal:
+                return curr_ratio
+            output = -1.0
+            for neigh in bwg[curr]:
+                output = dfs(neigh, goal, curr_ratio * bwg[curr][neigh], visited)
+                if output != -1.0:
+                    return output
+            return output 
+
+        output = []
+        for query in queries:
+            start, goal = query
+            output.append(dfs(start, goal, 1.0, set()))
+        return output
+            
